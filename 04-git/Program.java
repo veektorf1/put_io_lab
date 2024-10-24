5774178c1ee2c4a8a72ef81153a23aac17f27dd4
veektorf1 <wiktor.makowski@student.put.poznan.pl>
2024-10-24 17:40:46 +0200

    Edycja pliku Program.java

--- 04-git/Program.java
+++ 04-git/Program.java
@@ -2,7 +2,7 @@ public class Program {
 
     public static void main(String[] args) {
 
-        // Prints "Hello, World" in the terminal window.
+        // Prints "Hello, World" in the terminal window! 
         System.out.println("Hello, World!");
     }
 
