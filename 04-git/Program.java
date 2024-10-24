e3fe8c4f31ee09c081600035cc98999ba80c8d02
veektorf1 <wiktor.makowski@student.put.poznan.pl>
2024-10-24 17:38:30 +0200

    Zmiana w Program.java i README

--- 04-git/Program.java
+++ 04-git/Program.java
@@ -3,7 +3,7 @@ public class Program {
     public static void main(String[] args) {
 
         // Prints "Hello, World" in the terminal window.
-        System.out.println("Hello, World");
+        System.out.println("Hello, World!");
     }
 
 }
